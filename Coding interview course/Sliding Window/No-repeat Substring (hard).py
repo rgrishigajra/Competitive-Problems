9d0ed77c8c3b3b39e43186d758df6164ef6b112f
Rishabh gajra <rgrishigajra@gmail.com>
2020-07-14 13:57:53 -0400

    core: No-repeat Substring (hard) Adding more efficient code for the problem. Saved traversing time for pointer but the algo stays O(n). Time to solve<=10mins

--- Coding interview course/Sliding Window/No-repeat Substring (hard).py
+++ Coding interview course/Sliding Window/No-repeat Substring (hard).py
@@ -18,6 +18,20 @@ def non_repeat_substring(str):
             max_string_length = max(max_string_length, len(characters))
     return max_string_length
 
+#below solution instrad of removing the charac from the map, just remembers the last position of the character. So no need to traverse the window start pointer
+
+def non_repeat_substring_efficient(str1):
+    window_start = 0
+    max_length = 0
+    char_index_map = {}
+    for window_end in range(len(str1)):
+        right_char = str1[window_end]
+        if right_char in char_index_map:
+            window_start = max(window_start, char_index_map[right_char]+1)
+        char_index_map[right_char]=window_end
+        max_length=max(max_length,window_end-window_start+1)
+    return max_length
+
 
 def main():
     print("Length of the longest substring: " +
