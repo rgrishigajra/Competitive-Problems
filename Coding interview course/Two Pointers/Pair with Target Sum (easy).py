8ed628f82363cd6340e6bad545acbe78bf1b1454
Rishabh gajra <rgrishigajra@gmail.com>
2020-07-20 11:41:17 -0400

    feat: Pair with Target Sum (easy) ADding solution to first problem in two Sum. had done this before so was easy. Time to solve: 5mins

---
+++ Coding interview course/Two Pointers/Pair with Target Sum (easy).py
@@ -0,0 +1,17 @@
+#Given an array of sorted numbers and a target sum, find a pair in the array whose sum is equal to the given target.
+
+def pair_with_targetsum(arr, target_sum):
+  start_ptr=0
+  end_ptr=len(arr)- 1
+#   if the array isnt sorted, we need to sort the thing
+  while True:
+    if arr[start_ptr]+ arr[end_ptr]==target_sum:
+      return [start_ptr,end_ptr]
+    if arr[start_ptr]+ arr[end_ptr]>target_sum:
+      end_ptr-=1
+    if arr[start_ptr]+ arr[end_ptr]<target_sum:
+      start_ptr+=1
+
+def main():
+  print(pair_with_targetsum([1, 2, 3, 4, 6], 6))
+  print(pair_with_targetsum([2, 5, 9, 11], 11))
