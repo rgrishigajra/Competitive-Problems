c0a501b05a719a5c2bbca231a521604cc83373d0
Rishabh gajra <rgrishigajra@gmail.com>
2020-09-01 15:52:36 -0400

    feat:Improved intersection from n^2 to nlog(n)

--- Coding interview course/Merge Intervals/Conflicting Appointments (medium).py
+++ Coding interview course/Merge Intervals/Conflicting Appointments (medium).py
@@ -12,6 +12,15 @@ def can_attend_all_appointments(intervals):
     return True
 
 
+def can_attend_all_appointments(intervals):
+    intervals.sort(key=lambda x: x[0])
+    start, end = 0, 1
+    for i in range(1, len(intervals)):
+        if intervals[i][start] < intervals[i-1][end]:
+            return False
+    return True
+
+
 def main():
     print("Can attend all appointments: " +
           str(can_attend_all_appointments([[1, 4], [2, 5], [7, 9]])))
