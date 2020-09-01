6baadc015c10f744cc1147fb983d67f7ef57c0f1
Rishabh gajra <rgrishigajra@gmail.com>
2020-09-01 15:48:26 -0400

    feat:Add solution for Conflicting Appointments (medium) Added solution for Conflicting Appointments (medium). Easy considering its the most basic thing in this type. Time to solve <=5mins. Added my own solution for Intervals intersection after I redid it today.

---
+++ Coding interview course/Merge Intervals/Conflicting Appointments (medium).py
@@ -0,0 +1,24 @@
+# Given an array of intervals representing ‘N’ appointments, find out if a person can attend all the appointments.
+def can_attend_all_appointments(intervals):
+    start, end = 0, 1
+    for i, interval in enumerate(intervals):
+        iterator = i + 1
+        while iterator < len(intervals):
+            if interval[start] >= intervals[iterator][start] and interval[start] <= intervals[iterator][end]:
+                return False
+            if interval[end] >= intervals[iterator][start] and interval[end] <= intervals[iterator][end]:
+                return False
+            iterator += 1
+    return True
+
+
+def main():
+    print("Can attend all appointments: " +
+          str(can_attend_all_appointments([[1, 4], [2, 5], [7, 9]])))
+    print("Can attend all appointments: " +
+          str(can_attend_all_appointments([[6, 7], [2, 4], [8, 12]])))
+    print("Can attend all appointments: " +
+          str(can_attend_all_appointments([[4, 5], [2, 3], [3, 6]])))
+
+
+main()
--- Coding interview course/Merge Intervals/Intervals Intersection (medium).py
+++ Coding interview course/Merge Intervals/Intervals Intersection (medium).py
@@ -1,26 +1,16 @@
-# Given two lists of intervals, find the intersection of these two lists. Each list consists of disjoint intervals sorted on their start time.
 def merge(intervals_a, intervals_b):
     result = []
-    i, j, start, end = 0, 0, 0, 1
-    while i < len(intervals_a) and j < len(intervals_b):
-        # check if intervals overlap and intervals_a[i]'s start time lies within the other intervals_b[j]
-        a_overlaps_b = intervals_a[i][start] >= intervals_b[j][start] and \
-            intervals_a[i][start] <= intervals_b[j][end]
-
-        # check if intervals overlap and intervals_a[j]'s start time lies within the other intervals_b[i]
-        b_overlaps_a = intervals_b[j][start] >= intervals_a[i][start] and \
-            intervals_b[j][start] <= intervals_a[i][end]
-
-        # store the the intersection part
-        if (a_overlaps_b or b_overlaps_a):
-            result.append([max(intervals_a[i][start], intervals_b[j][start]), min(
-                intervals_a[i][end], intervals_b[j][end])])
-
-        # move next from the interval which is finishing first
-        if intervals_a[i][end] < intervals_b[j][end]:
-            i += 1
+    a, b, start, end = 0, 0, 0, 1
+    while a < len(intervals_a) and b < len(intervals_b):
+        is_intersection = intervals_a[a][start] <= intervals_b[b][start] and intervals_a[a][end] >= intervals_b[b][
+            start] or intervals_a[a][start] >= intervals_b[b][start] and intervals_a[a][start] <= intervals_b[b][end]
+        if is_intersection:
+            result.append([max(intervals_a[a][start], intervals_b[b][start]), min(
+                intervals_a[a][end], intervals_b[b][end])])
+        if intervals_a[a][end] > intervals_b[b][end]:
+            b += 1
         else:
-            j += 1
+            a += 1
     return result
 
 
