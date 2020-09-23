86a8580c88115e14e07e5afaa96938ab030a6659
Rishabh gajra <rgrishigajra@gmail.com>
2020-09-23 01:06:32 -0400

    fix:re code was not saved

---
+++ Coding interview course/Subsets/Subsets (easy).py
@@ -0,0 +1,23 @@
+def find_subsets(nums):
+  subsets = []
+  # start by adding the empty subset
+  subsets.append([])
+  for currentNumber in nums:
+    # we will take all existing subsets and insert the current number in them to create new subsets
+    n = len(subsets)
+    for i in range(n):
+      # create a new subset from the existing subset and insert the current element to it
+      set = list(subsets[i])
+      set.append(currentNumber)
+      subsets.append(set)
+
+  return subsets[1:]
+
+
+def main():
+
+  print("Here is the list of subsets: " + str(find_subsets([1, 3])))
+  print("Here is the list of subsets: " + str(find_subsets([1, 5, 3])))
+
+
+main()
