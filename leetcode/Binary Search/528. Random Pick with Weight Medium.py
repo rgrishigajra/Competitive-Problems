aaaa4d4644eca9d3968672557bc19f0737f63015
Rishabh gajra <rgrishigajra@gmail.com>
2020-10-01 16:56:58 -0400

    feat:528. Random Pick with Weight Medium Weird ass problem, not well defined. Time to solve<=10mins

---
+++ leetcode/Binary Search/528. Random Pick with Weight Medium.py
@@ -0,0 +1,30 @@
+class Solution:
+    def __init__(self, w: List[int]):
+        """
+        :type w: List[int]
+        """
+        self.prefix_sums = []
+        prefix_sum = 0
+        for weight in w:
+            prefix_sum += weight
+            self.prefix_sums.append(prefix_sum)
+        self.total_sum = prefix_sum
+
+    def pickIndex(self) -> int:
+        """
+        :rtype: int
+        """
+        target = self.total_sum * random.random()
+        # run a binary search to find the target zone
+        low, high = 0, len(self.prefix_sums)
+        while low < high:
+            mid = low + (high - low) // 2
+            if target > self.prefix_sums[mid]:
+                low = mid + 1
+            else:
+                high = mid
+        return low
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(w)
+# param_1 = obj.pickIndex()
