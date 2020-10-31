36505f2aa37d64a902e528342c3e41620146a36e
Rishabh gajra <rgrishigajra@gmail.com>
2020-10-31 16:50:30 -0400

    feat:Subset Sum (medium) Same same like prev but with in o(n).

---
+++ Coding interview course/Knapsack Dynamic Programming/Subset Sum (medium).py
@@ -0,0 +1,33 @@
+# Given a set of positive numbers, determine if a subset exists whose sum is equal to a given number ‘S’.
+
+
+def can_partition(num, sum):
+    n = len(num)
+    dp = [False for x in range(sum+1)]
+
+    # handle sum=0, as we can always have '0' sum with an empty set
+    dp[0] = True
+
+    # with only one number, we can have a subset only when the required sum is equal to its value
+    for s in range(1, sum+1):
+        dp[s] = num[0] == s
+
+    # process all subsets for all sums
+    for i in range(1, n):
+        for s in range(sum, -1, -1):
+            # if dp[s]==true, this means we can get the sum 's' without num[i], hence we can move on to
+            # the next number else we can include num[i] and see if we can find a subset to get the
+            # remaining sum
+            if not dp[s] and s >= num[i]:
+                dp[s] = dp[s - num[i]]
+
+    return dp[sum]
+
+
+def main():
+    print("Can partition: " + str(can_partition([1, 2, 3, 7], 6)))
+    print("Can partition: " + str(can_partition([1, 2, 7, 1, 5], 10)))
+    print("Can partition: " + str(can_partition([1, 3, 4, 8], 6)))
+
+
+main()
