a7323a82bf617b64fdff395eed85f43d556cdf11
Rishabh gajra <rgrishigajra@gmail.com>
2021-02-08 14:03:57 -0500

    feat:259. 3Sum Medium no sorting Sorting only the results before adding them to the final answer saves some time it looks like. Also used more memory for this solution.

--- leetcode/15. 3Sum Medium.py
+++ leetcode/15. 3Sum Medium.py
@@ -24,3 +24,16 @@ class Solution:
                     l += 1
                     r -= 1
         return ans
+class Solution:
+    def threeSum_hash_no_sort(self, nums: List[int]) -> List[List[int]]:
+        res, dups = set(), set()
+        seen = {}
+        for i, val1 in enumerate(nums):
+            if val1 not in dups:
+                dups.add(val1)
+                for j, val2 in enumerate(nums[i+1:]):
+                    complement = -val1 - val2
+                    if complement in seen and seen[complement] == i:
+                        res.add(tuple(sorted((val1, val2, complement))))
+                    seen[val2] = i
+        return res
