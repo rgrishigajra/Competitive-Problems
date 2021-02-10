58530a979f2cad1b46b5da1b98d05bf2f7414c2c
Rishabh gajra <rgrishigajra@gmail.com>
2021-02-10 14:17:17 -0500

    feat:344. Reverse String Easy I feel bad for even doing this. TTS:1 min T:O(N) M:O(1)

---
+++ leetcode/344. Reverse String Easy.py
@@ -0,0 +1,7 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        start, end = 0, len(s)-1
+        while start < end:
+            s[start], s[end] = s[end], s[start]
+            start += 1
+            end -= 1
