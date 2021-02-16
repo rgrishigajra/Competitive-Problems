3c9b2845a3ab7eb856b4a273f0c7ac946e37344b
Rishabh gajra <rgrishigajra@gmail.com>
2021-02-16 15:09:24 -0500

    feat:253. Meeting Rooms II Medium TTS 15 mins T:O(NlogN) M:O(N)

---
+++ Coding interview course/Merge Intervals/Minimum Meeting Rooms (hard).py
@@ -0,0 +1,40 @@
+from heapq import *
+
+
+class Meeting:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+
+    def __lt__(self, other):
+        return self.end < other.end
+
+
+def min_meeting_rooms(meetings):
+    if not meetings:
+        return 0
+    heap = []
+    max_len = 0
+    meetings.sort(key=lambda x: x.start)
+    for meeting in meetings:
+        while heap and meeting.start >= heap[0].end:
+            heappop(heap)
+        heappush(heap, meeting)
+        max_len = max(max_len, len(heap))
+    return max_len
+
+
+def main():
+    print("Minimum meeting rooms required: " + str(min_meeting_rooms(
+        [Meeting(4, 5), Meeting(2, 3), Meeting(2, 4), Meeting(3, 5)])))
+    print("Minimum meeting rooms required: " +
+          str(min_meeting_rooms([Meeting(1, 4), Meeting(2, 5), Meeting(7, 9)])))
+    print("Minimum meeting rooms required: " +
+          str(min_meeting_rooms([Meeting(6, 7), Meeting(2, 4), Meeting(8, 12)])))
+    print("Minimum meeting rooms required: " +
+          str(min_meeting_rooms([Meeting(1, 4), Meeting(2, 3), Meeting(3, 6)])))
+    print("Minimum meeting rooms required: " + str(min_meeting_rooms(
+        [Meeting(4, 5), Meeting(2, 3), Meeting(2, 4), Meeting(3, 5)])))
+
+
+main()
