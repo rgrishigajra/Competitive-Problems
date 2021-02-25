fd8935daca4d8d38ea0f824035982ec28f4ba458
Rishabh gajra <rgrishigajra@gmail.com>
2021-02-24 19:45:16 -0500

    feat:662. Maximum Width of Binary Tree Medium TTS: 15 mins T:O(N) M:O(N/2)

---
+++ leetcode/662. Maximum Width of Binary Tree Medium.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+class Solution:
+    def widthOfBinaryTree(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+
+        max_width = 0
+        # queue of elements [(node, col_index)]
+        queue = deque()
+        queue.append((root, 0))
+
+        while queue:
+            level_length = len(queue)
+            _, level_head_index = queue[0]
+            # iterate through the current level
+            for _ in range(level_length):
+                node, col_index = queue.popleft()
+                # preparing for the next level
+                if node.left:
+                    queue.append((node.left, 2 * col_index))
+                if node.right:
+                    queue.append((node.right, 2 * col_index + 1))
+
+            # calculate the length of the current level,
+            #   by comparing the first and last col_index.
+            max_width = max(max_width, col_index - level_head_index + 1)
+
+        return max_width
