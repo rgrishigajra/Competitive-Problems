56ce4be32c188e714f499cf6658182952c849bf6
Rishabh gajra <rgrishigajra@gmail.com>
2021-03-04 15:40:10 -0500

    feat:Given an array of n-1n−1 integers in the range from 11 to nn, find the one number that is missing from the array. Basic XOR. T:O(N) M:O(1)

---
+++ Coding interview course/Bitwise XOR/Intro.py
@@ -0,0 +1,23 @@
+# Given an array of n-1n−1 integers in the range from 11 to nn, find the one number that is missing from the array.
+def find_missing_number(arr):
+    n = len(arr) + 1
+    # x1 represents XOR of all values from 1 to n
+    x1 = 1
+    for i in range(2, n+1):
+        x1 = x1 ^ i
+
+    # x2 represents XOR of all values in arr
+    x2 = arr[0]
+    for i in range(1, n-1):
+        x2 = x2 ^ arr[i]
+
+    # missing number is the xor of x1 and x2
+    return x1 ^ x2
+
+
+def main():
+    arr = [1, 5, 2, 6, 4]
+    print('Missing number is:' + str(find_missing_number(arr)))
+
+
+main()
