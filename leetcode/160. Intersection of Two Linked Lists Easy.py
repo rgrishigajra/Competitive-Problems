b8c1e14627c52db020485be0e2ee2f8e774977b9
Rishabh gajra <rgrishigajra@gmail.com>
2021-03-05 21:45:02 -0500

    feat:160. Intersection of Two Linked Lists Easy Insane trick. Can do it by two traversals too which is also what this does.

---
+++ leetcode/160. Intersection of Two Linked Lists Easy.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+        pA = headA
+        pB = headB
+
+        while pA != pB:
+            pA = headB if pA is None else pA.next
+            pB = headA if pB is None else pB.next
+        return pA
